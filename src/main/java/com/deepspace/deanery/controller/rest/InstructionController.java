b318a89391aa1c2eee58ff56128067f2c3ae9a23
Новиков Евгений Алексеевич <eanovikov@phoenixit.ru>
2022-11-08 15:18:55 +0300

    @feature add html templates

--- src/main/java/com/deepspace/deanery/controller/rest/InstructionController.java
+++ src/main/java/com/deepspace/deanery/controller/rest/InstructionController.java
@@ -5,26 +5,25 @@ import com.deepspace.deanery.model.Instruction;
 import com.deepspace.dto.ExpulsionPercentageCourseResponse;
 import com.deepspace.dto.ExpulsionPercentageYearResponse;
 import com.deepspace.dto.projection.ShortInstructionDTO;
-import com.deepspace.dto.projection.ShortInstructionDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
 @Tag(name = "Instruction controller")
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/instruction")
 public class InstructionController {
@@ -33,8 +32,10 @@ public class InstructionController {
 
     @GetMapping("/expulsion/percentage/course")
     @Operation(summary = "Percentage of expulsion group by courses")
-    public List<ExpulsionPercentageCourseResponse> getResponse() {
-        return instructionService.getExpulsionPercentageByCourse();
+    public String getResponse(Model model) {
+        List<ExpulsionPercentageCourseResponse> expulsionPercentageByCourse= instructionService.getExpulsionPercentageByCourse();
+        model.addAttribute("expulsionPercentageByCourse", expulsionPercentageByCourse);
+        return "expulsion-list-by-course";
     }
 
     @GetMapping("/expulsion/percentage/year")
